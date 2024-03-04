7b45f374a5fa932e11ab9aab494d947acb345d1c
shaygitub <122000611+shaygitub@users.noreply.github.com>
2024-03-04 12:42:59 +0200

    Delete DrvLoader/DrvLoader directory

--- DrvLoader/DrvLoader/Common/ScopedResource/README.md
+++
@@ -1,4 +0,0 @@
-ScopedResource
-==============
-
-Scoped Resource - Generic RAII Wrapper for the Standard Library by Peter Sommerlad and Andrew L. Sandoval
--- DrvLoader/DrvLoader/clean.bat
+++
@@ -1,9 +0,0 @@
-del *.sdf
-del /a:h *.suo
-rmdir /s /q ipch
-rmdir /s /q Debug
-rmdir /s /q Release
-rmdir /s /q DrvLoader\Debug
-rmdir /s /q DrvLoader\Release
-del /s *.aps
-pause
